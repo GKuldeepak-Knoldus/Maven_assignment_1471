f41b3643ea177e9d3f7f6e6243bf5b3366125871
gkuldeepak <gupta.mag@gmail.com>
2021-01-17 17:51:33 +0530

    Submitted by Kuldeepak Gupta 1471

---
+++ assign/cal-api/src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void Division (int firstnum,int secondnum){
+        System.out.println("Division is "+ (firstnum / secondnum));
+    }
+
+    public static void Multiplication (int firstnum, int secondnum){
+        System.out.println("Multiplication is " + (firstnum * secondnum));
+    }
+
+    public static void Addition ( int firstnum, int secondnum){
+        System.out.println("Addition is " + (firstnum + secondnum));
+    }
+
+    public static void Substraction ( int firstnum, int secondnum){
+        System.out.println("Subtraction is " + (firstnum - secondnum));
+    }
+}
---
+++ assign/cal-impl/src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.Addition(20,10);
+        calculator.Substraction(20,10);
+        calculator.Multiplication(20,10);
+        calculator.Division(20,10);
+    }
+}
